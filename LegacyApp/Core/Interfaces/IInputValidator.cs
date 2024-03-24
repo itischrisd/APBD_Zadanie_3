91262bb7cea918774db5589f1ce3ed9c9a6d6287
Krzysztof Dudek <dudek.krzysztof94@gmail.com>
2024-03-24 19:02:52 +0100

    exercise 3 - during lesson

---
+++ LegacyApp/Core/Interfaces/IInputValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LegacyApp.Core.Interfaces;
+
+public interface IInputValidator
+{
+    public bool ValidateAge(DateTime dateOfBirth);
+    public bool ValidateEmail(string email);
+    public bool ValidateName(string firstName, string lastName);
+}
---
+++ LegacyApp/Core/Validators/Users/InputValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LegacyApp.Core.Interfaces;
+
+namespace LegacyApp.Core.Validators.Users;
+
+public class InputValidator : IInputValidator
+{
+    public bool ValidateName(string firstName, string lastName)
+    {
+        return !(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName));
+    }
+
+    public bool ValidateEmail(string email)
+    {
+        return email.Contains('@') && email.Contains('.');
+    }
+
+    public bool ValidateAge(DateTime dateOfBirth)
+    {
+        var now = DateTime.Now;
+        var age = now.Year - dateOfBirth.Year;
+        if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+        return age >= 21;
+    }
+}
